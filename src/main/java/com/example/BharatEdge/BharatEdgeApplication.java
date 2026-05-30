45b6eb0c6dd6b5d365efd2edcd9c80f7474ea598
abhay6200 <abhaypathak7321@gmail.com>
2026-05-30 09:53:22 +0530

    Initial BharatEdge Project

---
+++ src/main/java/com/example/BharatEdge/BharatEdgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BharatEdge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharatEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharatEdgeApplication.class, args);
+	}
+
+}
