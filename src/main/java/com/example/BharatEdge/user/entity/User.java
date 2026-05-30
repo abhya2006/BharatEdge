3de9edaad507712ee913a732b74065af8fbb291f
Chandan Pathak <150768996+pathakchandan321@users.noreply.github.com>
2026-05-30 11:17:04 +0530

    update

--- src/main/java/com/example/BharatEdge/user/entity/User.java
+++ src/main/java/com/example/BharatEdge/user/entity/User.java
@@ -1,4 +1,27 @@
 package com.example.BharatEdge.user.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+    String userName;
+    @Email
+    String emai;
+    @Size(min=10,max=10)
+    long number;
+    @Pattern(regexp = "^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@$!%?&])[A-Za-z\\d@$!%?&]{8,}$",
+            message = "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character")
+    String password;
 }
--- src/main/java/com/example/BharatEdge/user/repository/Repo.java
+++ src/main/java/com/example/BharatEdge/user/repository/Repo.java
@@ -1,4 +1,8 @@
 package com.example.BharatEdge.user.repository;
 
-public interface Repo {
+import com.example.BharatEdge.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repo extends JpaRepository<Long, User> {
+    public boolean findByUserName(String userName);
 }
